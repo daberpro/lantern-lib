4204fb8ba89269b17fc2bce0f127318a7c746c89
nianawati <166094740+nianawati@users.noreply.github.com>
2025-06-11 18:44:23 +0700

    update: Add file class wrapper for HDF5 to save model

--- FeedForwardNetwork/FeedForwardNetwork.h
+++ FeedForwardNetwork/FeedForwardNetwork.h
@@ -11,6 +11,7 @@
 #include "DataProcessing.h"
 #include "../Headers/Vector.h"
 #include "../Headers/Logging.h"
+#include "../Headers/File.h"
 
 namespace lantern {
 
@@ -34,6 +35,10 @@ namespace lantern {
             af::array output, target_output;
             
         public:
+
+            lantern::utility::Vector<af::array> GetParameters(){
+                return this->parameters;
+            }
 
             void SetInput(af::array* _input_data){
                 this->input_data = _input_data;
@@ -152,6 +157,113 @@ namespace lantern {
                         );
                     }
                 }
+            }
+
+            void SaveModel(const std::string& _path){
+                
+                lantern::file::LanternHDF5 model_saver_(_path);
+                model_saver_.Create();
+                model_saver_.GetAllData();
+                
+                /**
+                 * Parameter info
+                 * L{n} is a parameter at n layer
+                 * 
+                 * path "/Parameters" is a folder which contain all result of training parameters
+                 * 
+                */
+
+                // first create the group or folder for hold all parameters
+                model_saver_.CreateGroup("/Parameters");
+                model_saver_.SetActiveGroup("/Parameters"); // then set the group to be active group
+
+                std::string label_ = "param_";
+                std::string dataspace_name_;
+                std::string dataset_name_;
+                std::string output_node_type_;
+                uint32_t index = 0, rank = 1;
+                double* data = nullptr;
+                H5::StrType strType(H5::PredType::C_S1,H5T_VARIABLE);
+                lantern::utility::Vector<lantern::node::NodeType>* all_layer_type_ = this->layer->GetAllNodeTypeOfLayer();
+
+                for(af::array& param : this->parameters){
+
+                    data = param.host<double>();
+                    dataspace_name_ = label_+std::to_string(index);
+                    output_node_type_ = "OUTPUT_FUNCTION_SPACE_"+std::to_string(index);
+                    dataset_name_ = std::string("L") + std::to_string(index);
+
+                    // create dataspace for each array data
+                    rank = param.numdims();
+                    model_saver_.CreateDataSpace(dataspace_name_,rank,{
+                        static_cast<uint64_t>(param.dims(0)),
+                        static_cast<uint64_t>(param.dims(1)),
+                        static_cast<uint64_t>(param.dims(2)),
+                        static_cast<uint64_t>(param.dims(3))
+                    });
+
+                    model_saver_.CreateDataset(dataset_name_, dataspace_name_, H5::PredType::NATIVE_DOUBLE);
+                    model_saver_.PrintAllDatasets();
+                    model_saver_.WriteDataset(dataset_name_, data, H5::PredType::NATIVE_DOUBLE);
+
+                    model_saver_.CreateScalarDataSpace(output_node_type_);
+                    model_saver_.CreateAttributeAtDataset(
+                        dataset_name_,
+                        output_node_type_,
+                        "OUTPUT_FUNCTION_"+dataset_name_,
+                        strType
+                    );
+                    model_saver_.WriteAttributeAtDataset(
+                        dataset_name_,
+                        "OUTPUT_FUNCTION_"+dataset_name_,
+                        strType,
+                        lantern::node::GetNodeTypeAsString((*all_layer_type_)[index+1])
+                    );
+
+                    index++;
+                    af::freeHost(data);
+
+                }
+
+                model_saver_.CreateGroup("/ModelMetaData");
+                model_saver_.SetActiveGroup("/ModelMetaData");
+
+                auto* total_node_each_layer = this->layer->GetAllLayerSizes();
+
+                model_saver_.CreateDataSpace<2>("TOTAL_NODE_EACH_LAYER",{1,total_node_each_layer->size()});
+                model_saver_.CreateAttributeAtGroup(
+                    "/ModelMetaData",
+                    "TOTAL_NODE_EACH_LAYER",
+                    "TOTAL_NODE_EACH_LAYER",
+                    H5::PredType::NATIVE_UINT32
+                );
+                model_saver_.WriteAttributeAtGroup(
+                    "/ModelMetaData",
+                    "TOTAL_NODE_EACH_LAYER",
+                    H5::PredType::NATIVE_UINT32,
+                    total_node_each_layer->getData()
+                );
+
+                model_saver_.CreateScalarDataSpace("OUTPUT_PROBABILITY_FUNCTION");
+                model_saver_.CreateAttributeAtGroup(
+                    "/ModelMetaData",
+                    "OUTPUT_PROBABILITY_FUNCTION",
+                    "OUTPUT_PROBABILITY_FUNCTION",
+                    strType
+                );
+                model_saver_.WriteAttributeAtGroup(
+                    "/ModelMetaData",
+                    "OUTPUT_PROBABILITY_FUNCTION",
+                    strType,
+                    lantern::node::GetNodeTypeAsString(all_layer_type_->back()) // TODO this must get probability output function 
+                );
+
+
+            }
+
+            void LoadModel(const std::string& _path){
+                
+                
             }
 
         };
